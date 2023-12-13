6637c1278065f3cb4658b4814246526aba3d95a0
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-12-13 11:31:34 -0300

    updated

--- data.lua
+++ data.lua
@@ -1,6 +1,7 @@
 -- base
 require("data/itens.lua")
 require("data/pesquisa.lua")
+require("data/FD_group.lua")
 
 -- torres
 require("data/blocos/torre_sniper.lua")
---
+++ data/FD_group.lua
@@ -0,0 +1,62 @@
+data:extend(
+    {
+        -------------------------------------------------------------------------- FD-category
+        {
+            type = "item-group",
+            -- nome da categoria
+            name = "FD-category",
+            order = "e",
+            icon = "__Fortress-Defense-Mod__/graficos/technology/FD.png",
+            icon_size = 126,
+            icon_mipmaps = 2
+        },
+        {
+            type = "item-subgroup",
+            -- nome usado para definir o grupo dos itens
+            name = "auto-craft",
+            -- indexando o grupo a categoria
+            group = "FD-category",
+            -- ordem de A a Z e de cima para baixo para organização dos grupos
+            order = "FD-auto-craft"
+        },
+        {
+            type = "item-subgroup",
+            name = "component",
+            group = "FD-category",
+            order = "FD-component"
+        },
+        {
+            type = "item-subgroup",
+            name = "data-terminal",
+            group = "FD-category",
+            order = "FD-data-terminal"
+        },
+        {
+            type = "item-subgroup",
+            name = "processing",
+            group = "FD-category",
+            order = "FD-processing"
+        },
+        {
+            type = "item-subgroup",
+            name = "storage-ME",
+            group = "FD-category",
+            order = "FD-storage-ME"
+        },
+        {
+            type = "item-subgroup",
+            name = "resources-generated",
+            group = "FD-category",
+            order = "FD-resources-generated"
+        }
+    }
+)
+-------------------------------------------------------------------------- FD-category
+
+-- categorias de receitas
+data:extend({
+    {
+        type = "recipe-category",
+        name = "inscriber"
+    }
+})
